0689a92f029ff566ef94590436c34754e6dea870
davidkopparg <davidkopparg@gmail.com>
2021-04-04 11:10:00 -0300

    atomisado Main y SideBar del dashboard

---
+++ grupo_digitalsports/src/components/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import SideBar from "./SideBar";
+import Main from "./Main"
+function App() {
+
+    return (
+       
+        <>
+		
+        <SideBar />
+		<Main />
+		
+        </>
+
+    )
+}
+
+export default App;
