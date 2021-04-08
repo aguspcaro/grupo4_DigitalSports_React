0503955c72c0100c6f0e6809cbb600a62ad57bbb
Alejandro Widomlanski <widomlanski@hotmail.com>
2021-04-08 15:09:09 -0300

    se atomizo panel con childrens

--- grupo_digitalsports/src/components/Main/index.js
+++ grupo_digitalsports/src/components/Main/index.js
@@ -6,8 +6,10 @@ import product from "../../assets/images/product_dummy.svg";
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Data from "./Data/Data";
+import Titulo from './Data/Titulo';
 import Panel from "./Panel/Panel";
 import User from "./Users/Users";
+
 
 
 
@@ -23,11 +25,7 @@ const Main = () => {
 					
 				<div className="container-fluid">
 					
-					<div className="d-sm-flex align-items-center justify-content-between mb-4">
-
-						<h1 className="h3 mb-0 text-gray-800">Digital Sports</h1>
-
-					</div>
+					<Titulo title="Digital Sports"/ >
 
 					<div className="row">
 
@@ -42,37 +40,28 @@ const Main = () => {
 					<div className="row">
 					
 						<div className="col-lg-6 mb-4">
-
-							<div className="card shadow mb-4">
-
-								<div className="card-header py-3">
-
-									<h6 className="m-0 font-weight-bold text-primary">Last product in Data Dase</h6>
-
-								</div>
-
-								<div className="card-body">
-
-									<div className="text-center">
-
-										<img className="img-fluid px-3 px-sm-4 mt-3 mb-4" style={{width : "25rem"}} src={product} alt="dummy"/>
-
-									</div>
-
-									<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, consequatur explicabo officia inventore libero veritatis iure voluptate reiciendis a magnam, vitae, aperiam voluptatum non corporis quae dolorem culpa exercitationem ratione?</p>
-									
-									<a target="_blank" rel="nofollow" href="/">View product detail</a>
-
-								</div>
-
+							<Panel title="Last product in Data Dase">
+						    <div className="text-center">
+								<img className="img-fluid px-3 px-sm-4 mt-3 mb-4" style={{width : "25rem"}} src={product} alt="dummy"/>
 							</div>
-
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, consequatur explicabo officia inventore libero veritatis iure voluptate reiciendis a magnam, vitae, aperiam voluptatum non corporis quae dolorem culpa exercitationem ratione?</p>
+							<a target="_blank" rel="nofollow" href="/">View product detail</a>
+							</Panel>
 						</div>
 						
 						<div className="col-lg-6 mb-4">						
-							<Panel/>
-
-						</div>
+							<Panel title="Categories in Data Base">
+							<div className="row">
+						      <div className="col-lg-6 mb-4">
+							  	<div className="card bg-info text-white shadow">
+							     <div className="card-body">
+								   Category 01
+						     	</div>
+						        </div>
+					         </div>
+				          </div>
+							</Panel>
+					    </div>
 
 						<div className="col-lg-6 mb-4">
 											
