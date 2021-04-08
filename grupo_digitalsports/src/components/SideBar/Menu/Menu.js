2980abc54ddee367835264b2eb3831bdc143ca1b
Alejandro Widomlanski <widomlanski@hotmail.com>
2021-04-08 16:56:52 -0300

    se atomizo slidebar

---
+++ grupo_digitalsports/src/components/SideBar/Menu/Menu.js
@@ -0,0 +1,61 @@
+
+
+import React from 'react'
+import NavItem from './NavItem/NavItem'
+
+
+export default function Menu() {
+
+    return (
+		
+		<ul className="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+		<a class="sidebar-brand d-flex align-items-center justify-content-center" href="/">
+
+			<div class="sidebar-brand-icon">
+
+				<i class="fas fa-chart-line"></i>
+
+			</div>
+
+			<div class="sidebar-brand-text mx-3">Admin</div>
+
+		</a>
+		
+		<hr className="sidebar-divider my-0"/>		
+
+		<NavItem title="Dashboard" active='active'>
+
+			<i className="fas fa-fw fa-tachometer-alt"></i>
+
+		</NavItem>
+		
+		<hr className="sidebar-divider"/>
+		
+		<div className="sidebar-heading">Actions</div>	
+
+		<NavItem title="Pages">
+		
+			<i className="fas fa-fw fa-folder"></i>
+
+		</NavItem>
+
+		<NavItem title="Charts">
+		
+			<i className="fas fa-fw fa-chart-area"></i>
+
+		</NavItem>	
+
+		<NavItem title="Tables">
+		
+			<i className="fas fa-fw fa-table"></i>
+
+		</NavItem>		
+		
+		<hr className="sidebar-divider d-none d-md-block"/>
+
+	</ul>		
+
+
+    )
+}
--- grupo_digitalsports/src/components/SideBar/index.js
+++ grupo_digitalsports/src/components/SideBar/index.js
@@ -1,58 +1,12 @@
 import React from 'react'
 
-import NavItem from './NavItem/NavItem'
+import SlideBar from './Menu/Menu'
 
 export default function SideBar() {
 
     return (
 		
-        <ul className="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
-
-			<a class="sidebar-brand d-flex align-items-center justify-content-center" href="/">
-
-				<div class="sidebar-brand-icon">
-
-					<i class="fas fa-chart-line"></i>
-
-				</div>
-
-				<div class="sidebar-brand-text mx-3">Admin</div>
-
-			</a>
-			
-			<hr className="sidebar-divider my-0"/>		
-
-			<NavItem title="Dashboard" active='active'>
-
-				<i className="fas fa-fw fa-tachometer-alt"></i>
-
-			</NavItem>
-			
-			<hr className="sidebar-divider"/>
-			
-			<div className="sidebar-heading">Actions</div>	
-
-			<NavItem title="Pages">
-			
-				<i className="fas fa-fw fa-folder"></i>
-
-			</NavItem>
-
-			<NavItem title="Charts">
-			
-				<i className="fas fa-fw fa-chart-area"></i>
-
-			</NavItem>	
-
-			<NavItem title="Tables">
-			
-				<i className="fas fa-fw fa-table"></i>
-
-			</NavItem>		
-			
-			<hr className="sidebar-divider d-none d-md-block"/>
-
-		</ul>		
+        <SlideBar/>
 
     )
 }
