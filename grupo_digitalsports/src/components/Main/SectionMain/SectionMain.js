fd8de1cdb50143af2c98f696e9183e2419401863
davidkopparg <davidkopparg@gmail.com>
2021-04-10 18:04:42 -0300

    hook creado para cantidad de productos mostrar en componente!

--- grupo_digitalsports/src/components/Main/SectionMain/SectionMain.js
+++ grupo_digitalsports/src/components/Main/SectionMain/SectionMain.js
@@ -9,6 +9,10 @@ import User from "./Users/Users";
 import product from "../../../assets/images/product_dummy.svg";
 
 
+
+
+
+
 function useFetche(url, defaultResponse){ 
 
 	const [data, setData] = useState(defaultResponse) 
@@ -35,6 +39,24 @@ function useFetche(url, defaultResponse){
 }
 
 const SectionMain = () => { 
+  
+/*hook de total de productos    */
+const [productos, setProducto]=React.useState([])
+
+React.useEffect( () =>{
+	listadoProductos()
+},[])
+/*hook de total de productos    */
+
+/* hook productos llamado a la api    */
+    const listadoProductos= async()=>{
+		const data = await fetch ("http://localhost:3001/api/products")
+		const products = await data.json()
+		//console.log (products)
+		setProducto(products)
+}
+/* hook productos llamado a la api    */
+
 
     const apiEndpoint = 'http://localhost:3001/api/users/'
 
@@ -53,7 +75,7 @@ const SectionMain = () => {
             <div className="container-fluid">
                 <Titulo title="Digital Sports" />
             <div className="row">
-                <Data title="Total de productos" cant = "20"/>
+                <Data title="Total de productos" cant = {productos.meta.count}/>
                 <Data title="Total de usuarios" cant = {apiUser.count}/>
                 <Data title="Total de categorías" cant = "10"/>
             </div>					
